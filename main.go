ef0799e565697c8fa547ff2c33fc6cee9a525a65
Alexander F Rødseth <rodseth@gmail.com>
2016-07-08 14:48:40 +0200

    Make golint happy

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 )
 
 func main() {
-	eliza_name := "Eliza"
-	niall_name := "Niall"
+	elizaName := "Eliza"
+	niallName := "Niall"
 
 	niall.Init()
 
@@ -23,22 +23,22 @@ func main() {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 
-	eliza_says_hi := goeliza.ElizaHi()
-	fmt.Println(eliza_name + ": " + eliza_says_hi)
-	niall.Learn(eliza_says_hi)
+	elizaSaysHi := goeliza.ElizaHi()
+	fmt.Println(elizaName + ": " + elizaSaysHi)
+	niall.Learn(elizaSaysHi)
 
 	// Number of lines written by Niall
 	numlines := 10
 	if len(os.Args) > 1 {
-		numlines_string := os.Args[1]
-		if numlines_int, err := strconv.Atoi(numlines_string); err == nil {
-			numlines = numlines_int
+		numlinesString := os.Args[1]
+		if numlinesInt, err := strconv.Atoi(numlinesString); err == nil {
+			numlines = numlinesInt
 		}
 	}
 
 	for x := 0; x < numlines; x++ {
 		statement := niall.Talk()
-		fmt.Printf("%s: %s\n", niall_name, statement)
-		fmt.Printf("%s: %s\n", eliza_name, goeliza.ReplyTo(statement))
+		fmt.Printf("%s: %s\n", niallName, statement)
+		fmt.Printf("%s: %s\n", elizaName, goeliza.ReplyTo(statement))
 	}
 }
